c024c43a66b4f31ee8f3f095d44387b665edc357
Jacob Lizarraga <jacobl7893@gmail.com>
2023-02-13 20:03:04 -0800

    remove files

--- Exercise1.3.scm
+++
@@ -1,5 +0,0 @@
-#lang simply-scheme
-(define (sum-largest-squares a b c)
-  (cond ((and (> a b) (> b c)) (+ (* a a) (* b b)))
-        ((and (> a b) (> c b)) (+ (* a a) (* c c)))
-        (else (+ (* b b) (* c c)))))
