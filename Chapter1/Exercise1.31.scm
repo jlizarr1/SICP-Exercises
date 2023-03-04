b00940cbaf01e436db191cded189aa6d54511301
Jacob Lizarraga <jacobl7893@gmail.com>
2023-03-03 16:43:37 -0800

    finished exercise 1.31

--- Chapter1/Exercise1.31.scm
+++ Chapter1/Exercise1.31.scm
@@ -1,12 +1,14 @@
 #lang simply-scheme
 ;part a
+
+;recursive product
 (define (product term a next b)
   (if (> a b)
       1
       (* (term a)
          (product term (next a) next b))))
 
-;;factorial procedure using product
+;factorial procedure using product
 (define (factorial n)
   (define (increment x)
     (+ x 1))
@@ -14,5 +16,23 @@
     x)
   (product ident 1 increment n))
 
-;;(define (pi n)
-  
+(define (square x)
+  (* x x))
+
+;pi approximation using John Wallis formula
+(define (pi-prod a b)
+  (define (pi-term x)
+    (/(* 4 (square x)) (- (* 4 (square x)) 1)))
+  (define (increment x)
+    (+ x 1))
+  (* 2.0 (product pi-term a increment b)))
+
+;part b
+
+;iterative product
+(define (product-iter term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (* (term a) result))))
+  (iter a 1))
