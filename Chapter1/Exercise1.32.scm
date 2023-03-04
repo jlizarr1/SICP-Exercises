bacda1a7dfd29e131fa35d28f79cf7a51e8d9ad1
Jacob Lizarraga <jacobl7893@gmail.com>
2023-03-03 18:00:56 -0800

    finished exercise 1.32

---
+++ Chapter1/Exercise1.32.scm
@@ -0,0 +1,25 @@
+#lang simply-scheme
+;part a
+
+;accumulate recursive
+(define (accumulate combiner null-value term a next b)
+  (if (> a b)
+      null-value
+      (combiner (term a)
+                (accumulate combiner null-value term (next a) next b))))
+
+(define (sum term a next b)
+  (accumulate + 0 term a next b))
+
+(define (product term a next b)
+  (accumulate * 1 term a next b))
+
+;part b
+
+;accumulate iterative
+(define (accumulate-iter combiner null-value term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (combiner result (term a)))))
+  (iter a null-value))
